71fd2847e4d44db01978c28968a8fd9cf7464846
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2022-05-19 01:21:10 +0200

    :construction: Make summary statistics

--- analysis.py
+++ analysis.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-from microeconometrics.descriptives import get_summary
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import microeconometrics.descriptives as ds
 
 pd.options.display.max_columns = None
 pd.options.display.max_colwidth = None
 pd.options.display.width = None
-
 
 # Data prepartation
 data = pd.read_csv('data/data_group5.csv')
@@ -15,8 +15,14 @@ data.columns = [col.lower() for col in data.columns]
 data['higher_market_value_a'] = (data.value_a - data.value_h > 0).astype(int)
 
 # Get summary statistics
-h = get_summary(data.select_dtypes(include=['int64', 'float64']))
+ds.get_summary(data.select_dtypes(include=['int64', 'float64']))
+scatter_plot = ds.plot_scatter(x=data.higher_market_value_a, y=data.points_a)
 
+scatter_plot.figure.show()
 
+h = data.groupby(['team_h', 'team_a'])['points_h', 'points_a'].sum()
 
-ax.figure.show()
+pd.DataFrame({'team': pd.concat([data.team_h, data.team_a]), 'points': pd.concat([data.points_h, data.points_a])}).groupby('team').agg(['sum', 'mean', 'count'])
+
+for group, df in data.groupby(['team_h']):
+    df[['team_h', 'points_h', 'higher_market_value_a']].sum()
--- microeconometrics/descriptives.py
+++ microeconometrics/descriptives.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 
 # Summary statistics
@@ -28,12 +30,19 @@ def get_summary(df: pd.DataFrame) -> pd.DataFrame:
 
 
 # Plots
-def _jitter(values, j=0.01):
+def _jitter(values: pd.Series, j: float = 0.01) -> pd.Series:
+    """
+    Add variation to the values of a series for a jitter effect in the scatter plot.
+    """
     return values + np.random.normal(j, 0.1, values.shape)
 
 
 # Descriptive analysis
-def plot_scatter(x=pd.Series, y=pd.Series)
+def plot_scatter(x=pd.Series, y=pd.Series):
+    """
+    Draws a scatter plot of two series and adds a mean.
+    """
     ax = sns.scatterplot(x=_jitter(x), y=_jitter(y), alpha=0.2)
     plt.scatter(x=0, y=x.mean(), color='r')
-    plt.scatter(x=1, y=y.mean(s), color='r')
+    plt.scatter(x=1, y=y.mean(), color='r')
+    return ax
